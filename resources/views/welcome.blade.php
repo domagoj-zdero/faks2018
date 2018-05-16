2d7f989086404c53d55b509a2507903c59ca7b4e
domagoj-zdero <domagoj.zdero@gmail.com>
2018-05-16 20:57:13 +0200

    nesto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,9 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
+                <div class="links">
+                    <a href="{{ route('fakultet') }}">Fakultet</a>
+                </div><hr>
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
