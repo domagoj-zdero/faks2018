9a50bb3c83249a4d0628aede258794c0beb21bcf
domagoj-zdero <domagoj.zdero@gmail.com>
2018-05-16 19:16:05 +0200

    initial commit

---
+++ resources/views/fakultet.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Dobrodošli na fakultet</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>
+            Ovo je neki dio ali još nema veze sa laravelom
+
+        </div>
+    </body>
+</html>
+<?php
